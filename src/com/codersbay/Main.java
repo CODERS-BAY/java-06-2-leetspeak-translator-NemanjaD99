b441a116d1c450c3a1d75df2b83fe71500670dc8
Nemanja <nemanjadragomirovic086@gmail.com>
2021-02-25 13:59:47 +0100

    Implement Leetspeak translator

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -1,6 +1,130 @@
 package com.codersbay;
 
+
+import java.io.FileNotFoundException;
+
+
+
 public class Main {
 
 
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+        String letter ="hello world this text is made by sadow kantaro.";
+
+        if (letter.contains("a")) {
+            letter = letter.replace("a", "@");
+        }
+        if (letter.contains("b")) {
+            letter = letter.replace("b", "8");
+        }
+        if (letter.contains("c")) {
+            letter = letter.replace("c", "(");
+        }
+        if (letter.contains("d")) {
+            letter = letter.replace("d", "B");
+        }
+        if (letter.contains("e")) {
+            letter = letter.replace("e", "3");
+        }
+        if (letter.contains("f")) {
+            letter = letter.replace("f", "/=");
+        }
+        if (letter.contains("g")) {
+            letter = letter.replace("g", "6");
+        }
+        if (letter.contains("h")) {
+            letter = letter.replace("h", "#");
+
+        }
+        if (letter.contains("i")) {
+            letter = letter.replace("i", "!");
+
+        }
+        if (letter.contains("j")) {
+            letter = letter.replace("j", "l");
+
+        }
+        if (letter.contains("k")) {
+            letter = letter.replace("k", ")");
+
+        }
+        if (letter.contains("l")) {
+            letter = letter.replace("l", "J");
+
+        }
+        if (letter.contains("m")) {
+            letter = letter.replace("m", "W");
+
+        }
+        if (letter.contains("n")) {
+            letter = letter.replace("n", "//");
+        }
+
+        if (letter.contains("o")) {
+            letter = letter.replace("o", "0");
+        }
+
+        if (letter.contains("p")) {
+            letter = letter.replace("p", "q");
+        }
+
+        if (letter.contains("q")) {
+            letter = letter.replace("q", "O");
+        }
+
+        if (letter.contains("q")) {
+            letter = letter.replace("q", "F");
+        }
+
+        if (letter.contains("s")) {
+            letter = letter.replace("s", "Z");
+        }
+
+        if (letter.contains("t")) {
+            letter = letter.replace("t", "Y");
+        }
+
+        if (letter.contains("u")) {
+            letter = letter.replace("u", "C");
+        }
+
+        if (letter.contains("v")) {
+            letter = letter.replace("v", "U");
+        }
+
+        if (letter.contains("w")) {
+            letter = letter.replace("w", "M");
+        }
+        if (letter.contains("x")) {
+            letter = letter.replace("x", "*");
+        }
+
+        if (letter.contains("y")) {
+            letter = letter.replace("y", "V");
+        }
+
+        if (letter.contains("z")) {
+            letter = letter.replace("z", "S");
+        }
+        System.out.println(letter);
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
